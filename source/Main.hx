17de6ebe2816cab58f485afebaa7a887e9136796
Ethantobot <ethantobot7@gmail.com>
2026-08-25 03:16:00 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -98,19 +98,15 @@ class Main extends Sprite
 		// Gotta run this before any assets get loaded.
 		ModCore.initialize();
 
-		#if !mobile
 		fpsCounter = new KadeEngineFPS(10, 3, 0xFFFFFF);
 		bitmapFPS = ImageOutline.renderImage(fpsCounter, 1, 0x000000, true);
 		bitmapFPS.smoothing = true;
-		#end
 
-		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+		game = new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen);
 		addChild(game);
 
-		#if !mobile
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
-		#end
 
 		// Finish up loading debug tools.
 		Debug.onGameStart();
