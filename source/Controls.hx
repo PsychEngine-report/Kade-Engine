a7a0fa6a70ac18890fa98f603ad8ab3b7f2a194a
Ethantobot <ethantobot7@gmail.com>
2026-08-25 11:51:35 +0200

    nah i forgot xd

--- source/Controls.hx
+++ source/Controls.hx
@@ -11,6 +11,7 @@ import flixel.input.actions.FlxActionSet;
 import flixel.input.gamepad.FlxGamepadButton;
 import flixel.input.gamepad.FlxGamepadInputID;
 import flixel.input.keyboard.FlxKey;
+import flixel.util.FlxSave;
 
 #if (haxe >= "4.0.0")
 enum abstract Action(String) to String from String
