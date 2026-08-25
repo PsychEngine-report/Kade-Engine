ceab5429d2022499ff0b57992d0d4cd75a5d39ee
Ethantobot <ethantobot7@gmail.com>
2026-08-25 02:55:13 +0200

    Add showPopUp function for alert dialogs

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -48,4 +48,13 @@ class CoolUtil
 		}
 		return dumbArray;
 	}
+
+	public static function showPopUp(message:String, title:String):Void
+	{
+		/*#if android
+		android.Tools.showAlertDialog(title, message, {name: "OK", func: null}, null);
+		#else*/
+		FlxG.stage.window.alert(message, title);
+		//#end
+	}
 }
